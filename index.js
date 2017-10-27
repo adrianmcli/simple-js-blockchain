f093a78a28094a3dcd3b712f959f2e08c5954600
Adrian Li <li.adrianmc@gmail.com>
2017-10-27 01:53:37 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// TODO - define types in Flow
+// TODO - construct a genesis block
+// TODO - make a function that takes in a block and a transaction,
+// and returns a new block with the transaction added inside it
+// TODO - make a function that hashes a block (block -> string)
+// TODO - make a function that mines a block, or in other words:
+// takes in a block and an int (the nonce) and returns a new block
