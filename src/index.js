0944856bd3fc4c10be0c0193e23bed7c1c069e2d
Adrian Li <li.adrianmc@gmail.com>
2017-10-27 17:09:10 -0700

    add flow and declare types

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-// TODO - define types in Flow
+// @flow
+
+// 1. Define types in Flow
+
+type Transaction = {
+  from: string,
+  to: string,
+  amount: number
+}
+
+type Block = {
+  index: number,
+  txs: Transaction[],
+  hash: string,
+  prevHash: string,
+  nonce: ?number
+}
+
 // TODO - construct a genesis block
 // TODO - make a function that takes in a block and a transaction,
 // and returns a new block with the transaction added inside it
